4b1aa0bb849fe605442228ff7962269fa10840b0
icattlecoder <icattlecoder@gmail.com>
2014-05-29 14:27:09 +0800

    fix

--- app/insert/insert.go
+++ app/insert/insert.go
@@ -1,62 +1,109 @@
 package main
 
-import(
-		"log"
-		"fmt"
-		"os"
-		"github.com/jmhodges/levigo"
-		"flag"
-		"strconv"
-		"time"
-		"encoding/json"
+import (
+	"flag"
+	"fmt"
+	"github.com/jmhodges/levigo"
+	"github.com/sbunce/bson"
+	bson2 "labix.org/v2/mgo/bson"
+	"log"
+	"os"
+	"path/filepath"
+	"sync/atomic"
+	"time"
 )
 
 type M map[string]interface{}
 
 //
-func main(){
-	var limit = flag.Int("limit",1000,"limit result count")
+func main() {
+	var dbpath = flag.String("dbpath", "data", "")
+	var datasrc = flag.String("datasrc", "data", "")
+	var onlyKey = flag.Bool("onlyKey", false, "")
 	flag.Parse()
-	count := *limit
+
+	var count int64
+
 	opts := levigo.NewOptions()
-	opts.SetCache(levigo.NewLRUCache(3<<30))
+	opts.SetCache(levigo.NewLRUCache(3 << 30))
 	opts.SetCreateIfMissing(true)
-	db, err := levigo.Open("/tmp/leveldb", opts)
-	if err!=nil{
-			log.Println("levigo.Open :",err)
-			os.Exit(-1)
+	db, err := levigo.Open(*dbpath, opts)
+
+	if err != nil {
+		log.Println("levigo.Open :", err, *dbpath)
+		os.Exit(-1)
 	}
 	log.Println("Open leveldb success!")
 	defer db.Close()
 
+	if *datasrc == "" {
+		log.Println("datasrc cannot be empty")
+		os.Exit(-1)
+	}
+
+	go (func() {
+		for {
+			<-time.After(time.Minute)
+			fmt.Println(count)
+		}
+	})()
+
 	wo := levigo.NewWriteOptions()
 
-	basekey := "hello.jpg"
-
-	fmt.Println("starting insert",count,"items")
 	start := time.Now()
-	for i:=0;i<count;i++{
-		key := basekey + strconv.Itoa(i)
-		m:=M{
-			"owner":   23872243,
-			"itbl":     4000000,
-			"tbl":      "test",
-			"key":      key,
-			"fh":       []byte("abcdeffghijjklllmnopqrstuvwxyzxweowd1231212312xawbe"),
-			"hash":     "xhaxlakdffwezfljdlakdsfeoidf",
-			"fsize":    4000000,
-			"mimeType": "image/jpeg",
-			"putTime":  time.Now(),
-		}
-		bs,_ := json.Marshal(m)
 
-		if err = db.Put(wo,[]byte("7gr4wa:"+key),bs);err!=nil{
-				log.Println("db.Put",key,m)
+	filepath.Walk(*datasrc, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
 		}
-	}
+		fi, err2 := os.Open(path)
+		if err2 != nil {
+			return err2
+		}
+		for {
+			m, err2 := bson.ReadMap(fi)
+			if err2 != nil {
+				return nil
+			}
+
+			// _id, ok := m["_id"].(string)
+
+			_id := fmt.Sprint(m["_id"])
+
+			// log.Println(_id)
+
+			if *onlyKey {
+				if err2 = db.Put(wo, []byte(_id), []byte{}); err2 == nil {
+					atomic.AddInt64(&count, 1)
+				}
+			} else {
+
+				m2 := M{
+					"hash":     m["hash"],
+					"fsize":    m["fsize"],
+					"mimeType": m["mimeType"],
+					"fh":       m["fh"],
+					"putTime":  m["putTime"],
+				}
+				bs, err2 := bson2.Marshal(m2)
+
+				if err2 != nil {
+					log.Println("xxx")
+					continue
+				}
+				if err2 = db.Put(wo, []byte(_id), bs); err2 == nil {
+					atomic.AddInt64(&count, 1)
+				}
+			}
+		}
+
+		return nil
+
+	})
+
+	fmt.Println(count)
 
 	end := time.Now()
 	fmt.Println("end insert")
-	fmt.Println("Taken",end.Sub(start))
+	fmt.Println("Taken", end.Sub(start))
 }
-
